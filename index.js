515a370925cec2cfec1ea93339da427cca809c28
Raja H <rajahamid12.7@gmail.com>
2018-03-01 04:09:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
 	if (cardNumber) {
-		console.log('Your total cost is $' + total() + ', which will be charged to the card ' + cardNumber + '.')
+		console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
 		cart.splice(0)
 		console.log(cart)
 		}
